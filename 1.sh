db9e1404b053e6f0bf84899695a1e4b71b3ad319
takeshi2k <75792103+takeshi2k@users.noreply.github.com>
2021-11-15 06:24:00 +0900

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,3 +3,6 @@ apt install nmap autossh sshpass curl arial2
 
 
 https://docs.google.com/spreadsheets/d/1JE4GNWlfV_OA_K18dmTiQ9jtu0HPcKw17_Xdy0ip7rE/edit?usp=sharing
+
+https://storage.googleapis.com/ume-bucket/dnstest.sh
+https://storage.googleapis.com/ume-bucket/zdtdc_without_opt_91Fl.7z
