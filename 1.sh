c13e5dcdb47d3caedffd05c7beb6c5cb41cd2e93
takeshi2k <75792103+takeshi2k@users.noreply.github.com>
2021-11-22 21:19:17 +0900

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -6,3 +6,22 @@ https://docs.google.com/spreadsheets/d/1JE4GNWlfV_OA_K18dmTiQ9jtu0HPcKw17_Xdy0ip
 
 https://storage.googleapis.com/ume-bucket/dnstest.sh
 https://storage.googleapis.com/ume-bucket/zdtdc_without_opt_91Fl.7z
+
+hello-mpi.pbs
+hello-openmp-gcc.pbs
+hello-openmp-intel.pbs
+hello-serial.pbs
+hostname.pbs
+pbsify-master.zip
+Rbatch.pbs
+Rcross.pbs
+Rmulti.pbs
+submit.pbs
+supernova.pbs
+supernova_container.pbs
+supernova_orig.pbs
+trinity.pbs
+trinity_1.pbs
+trinity_2.pbs
+trinity_3.pbs
+
